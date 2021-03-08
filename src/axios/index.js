4c84d81c6ab9a0b10e743562c86a62d5d110dce5
Wlodzimierz Korza <wkorza@gmail.com>
2021-03-07 19:30:03 -0800

    Create axios instance

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL:'http://backend.dev.small-eod.siecobywatelska.pl/api/',
-    auth: {
-        userName: 'root',
-        password: 'root'
-}
+    baseURL:'http://backend.dev.small-eod.siecobywatelska.pl/api',
+    headers:{
+        'Access-Control-Allow-Origin': '*',
+    }
+
 })
 
 export default instance
