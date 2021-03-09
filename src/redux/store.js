b9c9747eba0cfc3adf224fdb2d0ee033acad31e9
Wlodzimierz Korza <wkorza@gmail.com>
2021-03-09 08:15:17 -0800

    Add  data to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,23 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import reducer from '../redux/reducers';
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import reducer from '../redux/reducers'
+import cases from '../dummydata/cases.json'
+import tags from '../dummydata/tags.json'
+import letters from '../dummydata/letters.json'
+import instytutions from '../dummydata/instytutions.json'
+import channels from '../dummydata/channels.json'
+import events from '../dummydata/events.json'
 
-const initialState = {};
+const initialState = {
+    cases: cases.results,
+    events: events.results,
+    channels: channels.results,
+    instytutions: instytutions.results,
+    letters: letters.results,
+    tags: tags.results,
+}
 
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunk)))
 
-export default store;
+export default store
