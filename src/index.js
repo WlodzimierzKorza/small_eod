105044009e146bcb2efca56d010c0e8580357e62
Wlodzimierz Korza <wkorza@gmail.com>
2021-03-08 17:52:09 -0800

    Add main css

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux'
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import App from './App'
 import store from './redux/store'
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import reportWebVitals from './reportWebVitals'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.scss'
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-        <App />
+            <App />
         </Provider>
     </React.StrictMode>,
     document.getElementById('root'),
-);
+)
 
-reportWebVitals();
+reportWebVitals()
