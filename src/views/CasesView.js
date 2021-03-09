ef83563628fbefef577fbbfb5be9c3f368dbe615
Wlodzimierz Korza <wkorza@gmail.com>
2021-03-09 08:17:34 -0800

    Minor fix

--- src/views/CasesView.js
+++ src/views/CasesView.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react'
 import AddButton from '../components/AddButton/AddButton'
+import Heading from '../components/Heading/Heading'
 import CasesList from '../components/Cases/CasesList'
 import CasesModal from '../components/Cases/CasesModal'
 
 const CasesView = () => {
     const [show, setShow] = useState(false)
-
     const handleClose = () => setShow(false)
     const handleShow = () => setShow(true)
 
     return (
         <>
-            <h2>Sprawy</h2>
+            <Heading>Sprawy</Heading>
             <CasesList />
             <CasesModal show={show} onHide={handleClose} />
             <AddButton method={handleShow} />
